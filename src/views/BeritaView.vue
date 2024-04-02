33683d20fb3710b63143534b2bd8f0fd35bd7c17
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-04-02 11:22:44 +0700

    judul berita

--- src/views/BeritaView.vue
+++ src/views/BeritaView.vue
@@ -3,6 +3,11 @@ import Berita from '@/components/sections/Berita.vue'
 </script>
 <template>
     <div>
+        <div class="pt-20 pb-10 px-6 max-w-max mx-auto">
+            <h2 class="text-3xl lg:text-5xl font-bold text-center text-blue-700">
+                Contoh  Berita
+            </h2>
+        </div>
 <Berita />
     </div>
 </template>
